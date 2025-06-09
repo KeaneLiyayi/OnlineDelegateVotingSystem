0f8c93229c42fe376a3ab18e0dcfdb01e37d0c93
Keane <keaneclement04@gmail.com>
2025-06-09 14:44:55 +0300

    Logiing in

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,33 +1,37 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-// Load environment variables from .env file
-dotenv.config();
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-    throw new Error("Please define the MONGODB_URI environment variable");
+  throw new Error("Please define the MONGODB_URI environment variable");
 }
 
-// Global caching for MongoDB connection
-let cached = global.mongoose || { conn: null, promise: null };
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
 export async function connectToDB() {
-    // If the connection is already cached, return it
-    if (cached.conn) {
-        return cached.conn;
-    }
+  if (cached.conn) {
+    return cached.conn;
+  }
+  console.log("Connecting to DB...")
 
-    // If no promise, initialize the connection
-    if (!cached.promise) {
-        cached.promise = mongoose.connect(MONGODB_URI, {
-            dbName: "voting_system", // Optional: If your DB has a specific name
-            useNewUrlParser: true,    // Deprecated but harmless
-            useUnifiedTopology: true // Deprecated but harmless
-        }).then((mongooseInstance) => mongooseInstance); // Wait for the connection to resolve
-    }
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      dbName: "voting_system",
+    });
+  }
 
-    cached.conn = await cached.promise; // Store the promise result in the cache
-    return cached.conn; // Return the connection object
+  try {
+    cached.conn = await cached.promise;
+    console.log("DB connected successfully");
+  } catch (err) {
+    console.log("DB connection error", err);
+    cached.promise = null;
+    throw err;
+  }
+
+  return cached.conn;
 }
